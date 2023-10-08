d022e5d99880ea939f987998eb097919ee2246e0
TTO-Owen <owen.keary@thetechout.com>
2023-10-08 16:16:20 +0100

    Rebuilt with email notifications and coloured output

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,50 @@
 import cron from 'node-cron';
 import axios from 'axios';
+import chalk from 'chalk';
+import * as postmark from 'postmark'
 
-const requestLinnworksSync = async () => {
+const stradaUrl = 'localhost:3000';
+
+let failure = 0;
+
+const requestLinnworksSync = async (time: string) => {
   try {
     const request = await axios
       .post(
-        'https://strada.thetechout.com/api/linnworks/sync/cled7kdse0000deq0ymdgv7au'
+        'http://' + stradaUrl + '/api/linnworks/sync/cled7kdse0000deq0ymdgv7au'
       )
       .then((response) => {
         return response.data;
       });
-    console.log('cled7kdse0000deq0ymdgv7au Order Sync. ' + request.message);
+    if(request.message == 'Success, No orders to create.') {
+      console.log(chalk.grey(`[${time}][${request.build}] cled7kdse0000deq0ymdgv7au Order Sync. ${request.message}`));
+    } else {
+      console.log(chalk.green(`[${time}][${request.build}] cled7kdse0000deq0ymdgv7au Order Sync. ${request.message} [${request.orders.length || "Unknown"} Orders Synced]`));
+    }
+    failure = 0;
   } catch (error) {
+    
+    if(failure != 1) {
+      emailError("Linnworks Order Sync Failure")
+      failure = 1;
+    }
     console.log(error);
-    console.log('Failed to request Linnworks Sync');
+    console.log(chalk.red('Failed to request Linnworks Sync'));
   }
 };
 
-cron.schedule(`*/1 7-19 * * *`, async () => {
-  console.log(`Runtime: Every Minute`);
-  requestLinnworksSync();
+cron.schedule(`*/0.5 7-19 * * *`, async () => {
+  const time = (new Date()).toISOString().slice(11,19);
+  await requestLinnworksSync(time);
 });
+
+const emailError = async (error_string: string) => {
+  let pmclient = new postmark.ServerClient("dc8a33e9-8c4c-4d7d-bca9-60aac0a0241c");
+  pmclient.sendEmail({
+    "From":"info@thetechout.com",
+    "To": "owen.keary@thetechout.com",
+    "Subject": "NodeCron Sync Failure",
+    "TextBody": error_string
+  })
+  return true;
+}
