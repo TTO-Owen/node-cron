336b4744b9cd40eecb004f937edc7552988b83e3
TTO-Owen <owen.keary@thetechout.com>
2023-10-11 09:09:36 +0100

    Removed email errors, will be alerted by betteruptime

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import cron from 'node-cron';
-import * as postmark from 'postmark'
 import { heartbeat, triggerHeartbeat } from './requests/heartbeat';
 
 import { requestAlgoliaSync } from './requests/algolia-sync'
@@ -7,38 +6,13 @@ import { requestLinnworksSync } from './requests/linnworks-sync'
 
 const stradaUrl = 'strada.thetechout.com';
 
-let failure = {
-  linnOrderSync: false,
-  algoliaOrderSync: false,
-};
-
 cron.schedule(`*/1 7-19 * * *`, async () => {
   const time = (new Date()).toISOString().slice(11,19);
   await triggerHeartbeat(heartbeat.nodeCron);
   const linnOrderSync = await requestLinnworksSync(stradaUrl, time);
-  if(linnOrderSync.success && !failure.linnOrderSync) {
-    emailError('Failed to Sync with Algolia', String(linnOrderSync.err));
-  }
-
-  failure.linnOrderSync = linnOrderSync.success;
 });
 
 cron.schedule(`*/10 * * * *`, async () => {
   const time = (new Date()).toISOString().slice(11,19);
   const algoliaSync = await requestAlgoliaSync(stradaUrl, time);
-  if(algoliaSync.success && !failure.algoliaOrderSync) {
-    emailError('Failed to Sync with Algolia', String(algoliaSync.err));
-  }
-  failure.algoliaOrderSync = algoliaSync.success;
 });
-
-const emailError = async (error_string: string, error: string) => {
-  let pmclient = new postmark.ServerClient("dc8a33e9-8c4c-4d7d-bca9-60aac0a0241c");
-  pmclient.sendEmail({
-    "From":"info@thetechout.com",
-    "To": "owen.keary@thetechout.com",
-    "Subject": "NodeCron Sync Failure",
-    "TextBody": `${error_string} \n\n ${error}`
-  })
-  return true;
-}
